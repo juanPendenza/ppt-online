c253f564bb88dbbdab50147559d075dfe29ef4d8
Juanpi <juan.pablo.pendenza.22@gmail.com>
2025-04-08 19:39:09 -0300

    Update server/db.

--- server/db.ts
+++ server/db.ts
@@ -1,8 +1,9 @@
 import * as admin from "firebase-admin";
-import * as serviceAccount from "./key.json";
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY_JSON);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as any),
+  credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://ppt-online-255de-default-rtdb.firebaseio.com",
 });
 
